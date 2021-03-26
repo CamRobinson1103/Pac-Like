cf3544e0325f8121cc552500c95c18f6cdd67a1a
Cameron Robinson <robinsoncameron1103@@gmail.com>
2021-03-25 23:54:16 -0500

    Added Cooties class

--- raygame/SeekPathBehavior.h
+++ raygame/SeekPathBehavior.h
@@ -6,6 +6,7 @@ class SeekPathBehavior : public PathfindBehavior
 public:
 	SeekPathBehavior(Maze* maze) :PathfindBehavior(maze) {}
 
+
 protected:
 	virtual MathLibrary::Vector2 findDestination(Agent* owner) override;
 };
